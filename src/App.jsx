6ddfac59db68312791d57407cfee31a29ab25dda
Juan Diaz <juanchodis@hotmail.com>
2023-12-18 07:28:30 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center">
+        <h1 className="text-3xl font-bold underline">Juan Diaz</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
